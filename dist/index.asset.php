d19e4cbdf2fc6c5b44a42705f85decd2a5f41770
Ajay Yadav <ajay.yadav@newfold.com>
2023-07-26 02:52:36 +0530

    press3-84 | wp scripts

---
+++ dist/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-api-fetch'), 'version' => 'd021496083a2aa6e451e');
--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,13 @@
 const path = require('path');
+const { merge } = require('webpack-merge');
+const wpScriptsConfig = require('@wordpress/scripts/config/webpack.config');
 
-module.exports = {
-  entry: './index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js',
-    library:"DescriptionGenerator",
-    libraryTarget: 'umd',
-    umdNamedDefine: true,
-    globalObject: 'this',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-react'],
-          },
-        },
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.js'],
-  },
-  externals: {
-    // Use external version of React
-    "react": {
-        "commonjs": "react",
-        "commonjs2": "react",
-        "amd": "react",
-        "root": "React"
-    },
-    "react-dom": {
-        "commonjs": "react-dom",
-        "commonjs2": "react-dom",
-        "amd": "react-dom",
-        "root": "ReactDOM"
+const aiUtilWebpackConfig = {
+    output: {
+        path: path.resolve(process.cwd(), `dist`),
+        library: ['newfold', 'wp-module-ai', '[name]'],
+        libraryTarget: 'window',
     }
-}
 };
+
+module.exports = merge(wpScriptsConfig, aiUtilWebpackConfig);
